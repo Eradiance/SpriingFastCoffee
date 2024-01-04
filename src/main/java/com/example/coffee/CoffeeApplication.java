13076b40d0593157db3d694d6f8e76c7a420be6f
ERadiance <d.radadev99@gmail.com>
2024-01-04 18:51:04 +0300

    Glava 5. Added Configurartion properties scan

--- src/main/java/com/example/coffee/CoffeeApplication.java
+++ src/main/java/com/example/coffee/CoffeeApplication.java
@@ -1,8 +1,13 @@
 package com.example.coffee;
 
+import com.example.coffee.model.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class CoffeeApplication {
 
@@ -10,4 +15,9 @@ public class CoffeeApplication {
         SpringApplication.run(CoffeeApplication.class, args);
     }
 
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
+    }
 }
